a5f67fcbbe64e6ea30a89f05db23fba9b978d83b
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-05-01 08:28:14 -0700

    src: fix for node 12.

--- src/node_unbound.cc
+++ src/node_unbound.cc
@@ -79,7 +79,8 @@ NodeUnbound::Init(v8::Local<v8::Object> &target) {
   v8::Local<v8::FunctionTemplate> ctor =
     Nan::New<v8::FunctionTemplate>(unbound_constructor);
 
-  target->Set(Nan::New("NodeUnbound").ToLocalChecked(), ctor->GetFunction());
+  target->Set(Nan::New("NodeUnbound").ToLocalChecked(),
+    Nan::GetFunction(ctor).ToLocalChecked());
 }
 
 NAN_METHOD(NodeUnbound::Version) {
